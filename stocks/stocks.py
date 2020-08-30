a6ab881b79080fd01a5eff1847c7e18f7e62285e
Pranshu Bansal <pranshu@Pranshus-MacBook-Pro.local>
2020-08-30 15:40:50 +0530

    final

--- stocks/stocks.py
+++ stocks/stocks.py
@@ -3,10 +3,11 @@ from selenium import webdriver
 import pandas as pd
 import csv, requests
 
+DECIMAL = 2
+
 op = pd.read_csv("links.csv")
-option_list = list(op[op.columns[1]])
-url_list = list(op[op.columns[2]])
-url = 'https://www.google.com/search?q=moneycontrol+'
+option_list = list(op[op.columns[3]])
+url_list = list(op[op.columns[4]])
 
 with open('stocks.csv', mode='w') as file:
     file.write("Option Symbol,Book Value,Market Value,Book/Market,EPS,Company P/E,Industry P/E\n")
@@ -14,24 +15,33 @@ with open('stocks.csv', mode='w') as file:
 for i, o in enumerate(option_list):
     o_url = url_list[i]
     response = requests.get(o_url)
-    soup = BeautifulSoup(response.content, "html.parser")
-    market = soup.find(id="div_nse_livebox_wrap").find("span", {"class": "span_price_wrap stprh red_hilight rdclr"})
-    if not market:
-        market = soup.find(id="div_nse_livebox_wrap").find("span", {"class": "span_price_wrap stprh grn_hilight grnclr"})
-    market = market.getText()
-    market = float(market) if market != '-' else 0
-    book = soup.findAll("div", {"class": "value_txtfr"})[2].getText()
-    book = float(book) if book != '-' else 0
-    eps = soup.findAll("div", {"class": "value_txtfr"})[6].getText()
-    eps = float(eps) if eps != '-' else 0
-    ind_pe = soup.findAll("div", {"class": "value_txtfr"})[5].getText()
-    ind_pe = float(ind_pe) if ind_pe != '-' else 0
-    pe = soup.findAll("div", {"class": "value_txtfr"})[1].getText()
-    pe = float(pe) if pe != '-' else 0
+    if response.content != b'' and response.url != "https://www.moneycontrol.com":     
+        soup = BeautifulSoup(response.content, "html.parser")
+        try:
+            market = soup.find(id="div_nse_livebox_wrap").find("span", {"class": "span_price_wrap stprh red_hilight rdclr"})
+            if not market:
+                market = soup.find(id="div_nse_livebox_wrap").find("span", {"class": "span_price_wrap stprh rdclr"})
+            if not market:
+                market = soup.find(id="div_nse_livebox_wrap").find("span", {"class": "span_price_wrap stprh grn_hilight grnclr"})
+            if not market:
+                market = soup.find(id="div_nse_livebox_wrap").find("span", {"class": "span_price_wrap stprh grnclr"})
+            market = market.getText()
+            market = float(market.replace(",", "")) if market != '-' else 0
+        except:
+            market = 0
+        book = soup.findAll("div", {"class": "value_txtfr"})[2].getText()
+        book = float(book.replace(",", "")) if book != '-' else 0
+        eps = soup.findAll("div", {"class": "value_txtfr"})[6].getText()
+        eps = float(eps.replace(",", "")) if eps != '-' else 0
+        ind_pe = soup.findAll("div", {"class": "value_txtfr"})[5].getText()
+        ind_pe = float(ind_pe.replace(",", "")) if ind_pe != '-' else 0
+        pe = soup.findAll("div", {"class": "value_txtfr"})[1].getText()
+        pe = float(pe.replace(",", "")) if pe != '-' else 0
+        bm = round(book/market, DECIMAL) if market != 0 else 0
 
-    with open('stocks.csv', mode='a') as file:
-        print(o)
-        file.write("{},{},{},{},{},{},{}\n".format(o, book, market, book/market, eps, pe, ind_pe))
+        with open('stocks.csv', mode='a') as file:
+            print(o)
+            file.write("{},{},{},{},{},{},{}\n".format(o, book, market, bm, eps, pe, ind_pe))
 
 
 
--- trades/trades.py
+++ trades/trades.py
@@ -1,73 +1,116 @@
 from bs4 import BeautifulSoup
 from selenium import webdriver
 import pandas as pd
-import csv
+import csv, requests
 
 DECIMAL = 1
-stocks = ["IBULHSGFIN", "SBIN", "DLF", "TATASTEEL", "JINDALSTEL", "BANDHANBNK", "PFC", "APOLLOTYRE", "CADILAHC", "M%26MFIN", "TATAMOTORS"]
+stocks = ["IBULHSGFIN", "SBIN", "DLF", "TATASTEEL", "JINDALSTEL", "BANDHANBNK", "PFC", "APOLLOTYRE", "CADILAHC", "M&MFIN", "TATAMOTORS"]
 percent_from_strike = 0.15
+expiry = "2020-09-24" #format "yyyy-mm-dd"
+
+links = pd.read_csv("links.csv")
 
 with open('trades.csv', mode='w') as file:
         file.write("\n")
 
 for s in stocks:
-    # url = "https://www1.nseindia.com/live_market/dynaContent/live_watch/option_chain/optionKeys.jsp?symbolCode=7057&symbol={}&symbol={}&instrument=OPTSTK&date=-&segmentLink=17&segmentLink=17".format(s, s)
-    url = "https://www1.nseindia.com/live_market/dynaContent/live_watch/option_chain/optionKeys.jsp?segmentLink=17&instrument=OPTSTK&symbol={}&date=24SEP2020".format(s, s)
-    browser = webdriver.Chrome("./chromedriver")
-    browser.get(url)
-    html = browser.page_source
-    browser.close()
+    print(s)
+    mc = list(links[links["StockCode"] == s]["MCCode"])[0]
+    if type(mc) == str:
+        # url = "https://www1.nseindia.com/live_market/dynaContent/live_watch/option_chain/optionKeys.jsp?segmentLink=17&instrument=OPTSTK&symbol={}&date=24SEP2020".format(s, s)
+        url = "https://www.moneycontrol.com/stocks/fno/view_option_chain.php?sc_id={}&sel_exp_date={}".format(mc, expiry)
+        response = requests.get(url)
+        soup = BeautifulSoup(response.content, "html.parser")
 
-    soup = BeautifulSoup(html, 'html.parser')
+        try:
+            curr_price = float(soup.findAll("p", {"class": "r_28 FL"})[0].find("strong").text.strip())
+        except:
+            curr_price = float(soup.findAll("p", {"class": "gr_28 FL"})[0].find("strong").text.strip())
+        lower = (1-percent_from_strike)*curr_price
+        upper = (1+percent_from_strike)*curr_price
 
-    curr_price = float(soup.find('b').text.strip().split()[1])
-    lower = (1-percent_from_strike)*curr_price
-    upper = (1+percent_from_strike)*curr_price
+        data = []
+        columns = []
+        table = soup.find('table', {"class": "tblopt"})
+        rows = table.findAll('tr')
+        cols = rows[1].findAll('th')
+        cols = [ele.text.strip() for ele in cols]
+        columns = [ele for ele in cols if ele]
+        columns.insert(5, 'Strike Price')
+        table = soup.findAll('table', {"class": "tblopt"})[1]
+        rows = table.findAll('tr')
+        for row in rows:
+            cols = row.findAll('td')
+            cols = [ele.text.strip().replace(',', '') for ele in cols if ele.text.strip()]
+            data.append([float(ele) if ele != '-' else 0 for ele in cols]) # Get rid of empty values
+        df = pd.DataFrame(data, columns=columns)
 
-    data = []
-    columns = []
-    table = soup.find('table', id="octable")
-    table_head = table.find('thead')
-    rows = table_head.find_all('tr')
-    cols = rows[1].find_all('th')
-    cols = [ele.text.strip() for ele in cols]
-    columns = [ele for ele in cols if ele]
-    table_body = table.find('tbody')
-    rows = table_body.find_all('tr')
-    for row in rows[2:-1]:
-        cols = row.find_all('td')
-        cols = [ele.text.strip().replace(',', '') for ele in cols if ele.text.strip()]
-        data.append([float(ele) if ele != '-' else 0 for ele in cols]) # Get rid of empty values
-    df = pd.DataFrame(data, columns=columns[1:-1])
+        relevant = df[df['Strike Price'].between(lower, upper)]
+        put_relevant = df[df['Strike Price'].between(lower, curr_price)]
+        call_relevant = df[df['Strike Price'].between(curr_price, upper)]
+        put_relevant = put_relevant.iloc[:, [5,6]].round(DECIMAL).iloc[::-1]
+        call_relevant = call_relevant.iloc[:, [5,0]].round(DECIMAL).iloc[::-1]
 
-    relevant = df[df['Strike Price'].between(lower, upper)]
-    put_relevant = df[df['Strike Price'].between(lower, curr_price)]
-    call_relevant = df[df['Strike Price'].between(curr_price, upper)]
-    relevant = relevant.iloc[:, [4,6,7,8,9,10,11,12,13,14,16]]
-    put_relevant = put_relevant.iloc[:, [10,12,13]]
-    call_relevant = call_relevant.iloc[:, [7,8,10]]
-    put_relevant['AvgPrice'] = (put_relevant["BidPrice"] + put_relevant["AskPrice"])/2
-    put_relevant2 = put_relevant.iloc[:, [0,3]].round(DECIMAL)
-    call_relevant['AvgPrice'] = (call_relevant["BidPrice"] + call_relevant["AskPrice"])/2
-    call_relevant2 = call_relevant.iloc[:, [2, 3]].round(DECIMAL)
+        with open('trades.csv', mode='a') as file:
+            file.write("{}\n".format(s))
+        call_relevant.to_csv('trades.csv', mode='a', index=False)
+        with open('trades.csv', mode='a') as file:
+            file.write("{},-,-,-,-,-,-,-,-,CURRENT PRICE\n".format(curr_price))
+        put_relevant.to_csv('trades.csv', mode='a', index=False, header=False)
+        with open('trades.csv', mode='a') as file:
+            file.write("\n")
+            file.write("\n")
+            file.write("\n")
+            file.write("\n")
 
-    with open('trades.csv', mode='a') as file:
-        file.write("{}\n".format(s))
-    put_relevant2.to_csv('trades.csv', mode='a', index=False)
-    with open('trades.csv', mode='a') as file:
-        file.write("{},-,-,-,-,-,-,-,-,CURRENT PRICE\n".format(curr_price))
-    call_relevant2.to_csv('trades.csv', mode='a', index=False, header=False)
-    with open('trades.csv', mode='a') as file:
-        file.write("\n")
-        file.write("\n")
-        file.write("\n")
-        file.write("\n")
+    else:
+        url = "https://www1.nseindia.com/live_market/dynaContent/live_watch/option_chain/optionKeys.jsp?segmentLink=17&instrument=OPTSTK&symbol={}&date=24SEP2020".format(s, s)
+        browser = webdriver.Chrome("./chromedriver")
+        browser.get(url)
+        html = browser.page_source
+        browser.close()
 
+        soup = BeautifulSoup(html, 'html.parser')
 
+        curr_price = float(soup.find('b').text.strip().split()[1])
+        lower = (1-percent_from_strike)*curr_price
+        upper = (1+percent_from_strike)*curr_price
 
+        data = []
+        columns = []
+        table = soup.find('table', id="octable")
+        table_head = table.find('thead')
+        rows = table_head.find_all('tr')
+        cols = rows[1].find_all('th')
+        cols = [ele.text.strip() for ele in cols]
+        columns = [ele for ele in cols if ele]
+        table_body = table.find('tbody')
+        rows = table_body.find_all('tr')
+        for row in rows[2:-1]:
+            cols = row.find_all('td')
+            cols = [ele.text.strip().replace(',', '') for ele in cols if ele.text.strip()]
+            data.append([float(ele) if ele != '-' else 0 for ele in cols]) # Get rid of empty values
+        df = pd.DataFrame(data, columns=columns[1:-1])
 
+        relevant = df[df['Strike Price'].between(lower, upper)]
+        put_relevant = df[df['Strike Price'].between(lower, curr_price)]
+        call_relevant = df[df['Strike Price'].between(curr_price, upper)]
+        relevant = relevant.iloc[:, [4,6,7,8,9,10,11,12,13,14,16]]
+        put_relevant = put_relevant.iloc[:, [10,12,13]]
+        call_relevant = call_relevant.iloc[:, [7,8,10]]
+        put_relevant['AvgPrice'] = (put_relevant["BidPrice"] + put_relevant["AskPrice"])/2
+        put_relevant2 = put_relevant.iloc[:, [0,3]].round(DECIMAL)
+        call_relevant['AvgPrice'] = (call_relevant["BidPrice"] + call_relevant["AskPrice"])/2
+        call_relevant2 = call_relevant.iloc[:, [2, 3]].round(DECIMAL)
 
-
-
-
-
+        with open('trades.csv', mode='a') as file:
+            file.write("{}\n".format(s))
+        put_relevant2.to_csv('trades.csv', mode='a', index=False)
+        with open('trades.csv', mode='a') as file:
+            file.write("{},-,-,-,-,-,-,-,-,CURRENT PRICE\n".format(curr_price))
+        call_relevant2.to_csv('trades.csv', mode='a', index=False, header=False)
+        with open('trades.csv', mode='a') as file:
+            file.write("\n")
+            file.write("\n")
+            file.write("\n")
+            file.write("\n")
